67543b33960c29ed4bcc6cd0834f709583dd8e99
ShroukOuda <s36568961@gmail.com>
2025-05-01 18:22:45 +0300

    delete comments in config file

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') #or 'hard-to-guess-string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or 'mysql+mysqlconnector://root:682022@localhost/gym_management'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
